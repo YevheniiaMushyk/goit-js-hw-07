27bad1272cf374cf02d8894f4fcb66391ca07c45
YevheniiaMushyk <142007023+YevheniiaMushyk@users.noreply.github.com>
2023-12-14 22:33:17 +0200

    add change task-2

--- js/task-6.js
+++ js/task-6.js
@@ -1,20 +1,47 @@
 const block = document.querySelector("#controls");
 const inputSell = block.firstElementChild;
 inputSell.addEventListener("change", (event) => {
-	return event.currentTarget.value;
+	return inputSell.value;
 });
-const createBlock = inputSell.nextElementSibling;
-const createBlockMarkup = [];
+
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
+}
+
+let minWidth = 30;
+let minHeight = 30;
 function createBoxes(amount) {
+	const boxes = document.querySelector("#boxes");
 	for (let i = 1; i <= amount; i += 1) {
-		createBlockMarkup.push(`<div id="boxes"></div>`);
+		const box = document.createElement("div");
+		box.classList.add("boxClass");
+		box.style.width = `${minWidth}px`;
+		box.style.height = `${minHeight}px`;
+		box.style.marginTop = `5px`;
+		box.style.backgroundColor = getRandomHexColor();
+		block.append(box);
+		minWidth += 10;
+		minHeight += 10;
 	}
 }
-createBlock.addEventListener("click", (event) => {
-	if (inputSell >= 1 && inputSell <= 100) {
-		block.insertAdjacentHTML("beforeend", createBoxes(inputSell));
+const createBlock = inputSell.nextElementSibling;
+createBlock.addEventListener("click", () => {
+	if (inputSell.value >= 1 && inputSell.value <= 100) {
+		createBoxes(inputSell.value);
+		const inputDelSell = block.firstElementChild;
+		inputDelSell.reset();
 	} else {
 		alert("Invalid value");
 	}
-	inputSell.reset();
+});
+
+function destroyBoxes() {
+	const deletBoxes = document.querySelector("#boxes");
+	deletBoxes.children.innerHTML("beforeend", "");
+}
+const deleteBlock = block.LastElementChild;
+deleteBlock.addEventListener("click", () => {
+	destroyBoxes();
 });
